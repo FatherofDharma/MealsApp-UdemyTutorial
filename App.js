0bbd87d71517d8b189cd2998dbd79c2b9169ddc9
FatherofDharma <fatherofdharma@gmail.com>
2019-10-07 13:47:54 -0700

    finish lesson 120

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
-
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import { useScreens } from "react-native-screens";
+
 import MealsNavigator from "./navigation/MealsNavigator";
+
+//done for performance purposes: use npm install --save react-native-screens----LESSON 119 from Udemy course
+useScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,18 +1,37 @@
+import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import Colors from "../constants/Colors";
 
-const MealsNavigator = createStackNavigator({
-  //shortcut version:
-  Categories: CategoriesScreen,
-  //longer version:
-  CategoryMeals: {
-    screen: CategoryMealsScreen
+const MealsNavigator = createStackNavigator(
+  {
+    //shortcut version:
+    Categories: {
+      screen: CategoriesScreen
+    },
+    //longer version:
+    CategoryMeals: {
+      screen: CategoryMealsScreen
+    },
+    MealDetail: MealDetailScreen
   },
-  MealDetail: MealDetailScreen
-});
+  {
+    //can set an inital route specifically instead of the defaul using the first item in the stack list above:
+    //initialRouteName: 'MealDetail' (just an example intial route other than the first one listed)
+    initialRouteName: "Categories",
+    //will change screen change style to modal on iOS, if you set mode:modal
+    mode: "modal",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+    }
+  }
+);
 
 export default createAppContainer(MealsNavigator);
