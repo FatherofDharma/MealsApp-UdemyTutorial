73e8f3cc452a0c8e4b4ec1bc7a0ebfa6a6e1b8e9
FatherofDharma <fatherofdharma@gmail.com>
2019-10-07 11:38:17 -0700

    finish lesson 115

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = itemData => {
+  return (
+    <View style={Styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = props => {
   return (
-    <View style={Styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go to Meals!"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-          //alternate and shorter way to write this with no functional difference:
-          //props.navigation.navigate('CategoryMeals');
-          //-------Other options to 'navigate'
-          // props.navigation.push("CategoryMeals");
-          //allows you to push to the same screen again, where as navigate will not let you
-          // props.navigation.replace("CategoryMeals");
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
@@ -26,6 +27,11 @@ const Styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
